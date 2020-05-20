7c7c7e723864e8e4230c4c7a34f18f36526ff6e6
TNX2021 <tvdnx2021@gmail.com>
2020-05-20 17:03:41 +0300

    Fixes

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,4 @@
 export function home() {
-    let template = '<h3 style="text-align: center">Лабораторная работа №4</h3> </br>' +
-        '<h4 style="text-align: center">Выполнил:студент группы М3307, Тавадян Алексей</h4>'
+    let template = '<h3 style="text-align: center">Лабораторная работа №4</h3><br><h4 style="text-align: center">Работу выполнил: Тавадян Алексей</h4>'
     app.innerHTML = template
 }
